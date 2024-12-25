7526c6c6f2f5d6a39c13d9b0a4835fb8d09e01c5
duckyies <variyathabhinav@gmail.com>
2024-12-25 21:54:10 +0530

    Basic linkedlist

---
+++ LinkedList.java
@@ -0,0 +1,37 @@
+public class LinkedList {
+
+    private Node head;
+
+    private class Node {
+
+        private int val;
+        private Node next;
+
+        Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        Node(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+// methods to add for now
+    // add to the front
+    // add to the end
+    // add at a specific index
+    // remove from the front
+    // remove from the end
+    // remove from a specific index
+    // get the size of the list
+    // get the value at a specific index
+    // check if the list is empty
+    // clear the list
+    // print the list
+    // reverse the list
+    // sort the list
+    // check if the list contains a value
+    // get the index of a value
+// bro im dead 😭😭😭
+}
