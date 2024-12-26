854fe03af6e5483de5f545f1efecdb528c56287a
duckyies <variyathabhinav@gmail.com>
2024-12-26 21:40:04 +0530

    add some methods

--- LinkedList.java
+++ LinkedList.java
@@ -1,18 +1,20 @@
-public class LinkedList {
+import java.util.ArrayList;
 
-    private Node head;
+public class LinkedList<T> {
 
-    private class Node {
+    private Node<T> head;
 
-        private int val;
-        private Node next;
+    private class Node<U> {
 
-        Node(int val, Node next) {
+        private U val;
+        private Node<U> next;
+
+        Node(U val, Node<U> next) {
             this.val = val;
             this.next = next;
         }
 
-        Node(int val) {
+        Node(U val) {
             this.val = val;
             this.next = null;
         }
@@ -22,16 +24,70 @@ public class LinkedList {
         this.head = null;
     }
 
-    LinkedList(int val) {
-        this.head = new Node(val);
+    LinkedList(T val) {
+        this.head = new Node<T>(val);
     }
 
+    public void addFront(T val) {
+        Node<T> newNode = new Node<T>(val);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addEnd(T val) {
+        Node<T> newNode = new Node<T>(val);
+
+        newNode.next = null;
+        if (head == null) {
+            head = newNode;
+        }
+
+        else {
+            Node<T> temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void addIndex(T val, int index) {
+        Node<T> newNode = new Node<T>(val);
+
+        if(index == 0) {
+            addFront(val);
+            return;
+        }
+        Node<T> tempNode = head;
+
+        while(index != 1) {
+            if(tempNode.next == null) {
+                throw new Error("Index out of bounds");
+            }
+            tempNode = tempNode.next;
+            index--;
+        }
+
+        Node<T> temp = tempNode.next;
+        tempNode.next = newNode;
+        newNode.next = temp;
+
+    }
+
+    public void printList() {
+        Node<T> temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    pu
 
 // methods to add for now
-    // add to the front
     // linkedlist from array
-    // add to the end
-    // add at a specific index
     // remove from the front
     // remove from the end
     // remove from a specific index
@@ -39,10 +95,8 @@ public class LinkedList {
     // get the value at a specific index
     // check if the list is empty
     // clear the list
-    // print the list
     // reverse the list
     // sort the list
     // check if the list contains a value
     // get the index of a value
-// bro im dead 😭😭😭
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
-import java.util.Arrays;
 public class Main {
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
+
+        LinkedList ll = new LinkedList();
+        ll.addFront();
+        ll.addFront(34);
+        ll.addFront(2);
+        ll.addEnd(100);
+        ll.addIndex(0,4);
+        ll.printList();
+
     }
 }
